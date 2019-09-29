fec76ec1f6c6ae874f836bdb3f8bdd8a279bd368
Delaby Pierre <delaby.pierre@gmail.com>
2019-09-29 15:48:06 +0200

    jacoco and conf

---
+++ src/test/java/com/pdy/demo/SmokeIT.java
@@ -0,0 +1,17 @@
+package com.pdy.demo;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
+@SpringBootTest
+public class SmokeIT {
+
+    @Test
+    public void applicationCanStartWithoutErrors() {
+    }
+
+}
