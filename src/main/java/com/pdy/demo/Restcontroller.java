8dccf648fdc42f55dec14c30a8ece73507848c9d
Delaby Pierre <delaby.pierre@gmail.com>
2019-09-29 15:15:40 +0200

    sonar conf

--- src/main/java/com/pdy/demo/Restcontroller.java
+++ src/main/java/com/pdy/demo/Restcontroller.java
@@ -1,5 +1,7 @@
 package com.pdy.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Restcontroller {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Restcontroller.class);
+
     @GetMapping("/api/test")
     public ResponseEntity<String> getHi(){
+        LOGGER.warn("test");
         return ResponseEntity.ok("ok");
     }
 
